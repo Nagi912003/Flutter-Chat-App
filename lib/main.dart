2c06e188659da3c4630e573a7c3ce196a6fc07b1
Nagi Mohamed <117310405+nagiElshershaby@users.noreply.github.com>
2023-05-23 13:45:46 +0300

    tried to add a splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
-import 'package:chat_app/screens/ath_screen.dart';
-//import 'package:chat_app/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+import 'package:chat_app/screens/ath_screen.dart';
+import 'package:chat_app/screens/splash_screen.dart';
 import 'screens/chat_screen.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await Firebase.initializeApp();
+
+  // await Future.delayed(const Duration(seconds: 2));
+  FlutterNativeSplash.remove();
   runApp(const MyApp());
 }
 
@@ -18,40 +23,27 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter chat',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        // textTheme: TextTheme.lerp(
-        //   ThemeData.light().textTheme.copyWith(
-        //         displayLarge: const TextStyle(
-        //           color: Colors.black,
-        //           fontSize: 40,
-        //           fontWeight: FontWeight.bold,
-        //         ),
-        //         displaySmall: const TextStyle(
-        //           color: Colors.black,
-        //           fontSize: 20,
-        //           fontWeight: FontWeight.bold,
-        //         ),
-        //         bodyLarge: const TextStyle(
-        //           color: Colors.black,
-        //           //fontSize: 20,
-        //           fontWeight: FontWeight.normal,
-        //         ),
-        //       ),
-        //   ThemeData.dark().textTheme,
-        //   1,
-        // ),
-        useMaterial3: true,
-      ),
-      home: StreamBuilder(stream: FirebaseAuth.instance.authStateChanges(),builder: (context,snapshot){
-        if(snapshot.hasData){
-          return const ChatScreen();
-        }
-        return const AuthScreen();
-      },),
-    );
+
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter chat',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home:StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context,snapshot){
+            if(snapshot.connectionState == ConnectionState.waiting){
+              return const SplashScreen();
+            }
+            if(snapshot.hasData){
+              FlutterNativeSplash.remove();
+              return const ChatScreen();
+            }
+            return const AuthScreen();
+          },),
+
+      );
   }
 }
